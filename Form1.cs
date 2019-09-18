ac0d477ee1f76eaf8025acb45392248af2c5df89
IgnatovaAnastasia <31617636+IgnatovaAnastasia@users.noreply.github.com>
2019-09-18 10:41:54 +0300

    созданы формы

--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace cg_lab3
 {
-    public partial class Form1 : Form
+    public partial class Loader : Form
     {
-        public Form1()
+        public Loader()
         {
             InitializeComponent();
+        }
+
+        private void Task1bClick(object sender, EventArgs e)
+        {
+            (new Task1b.Task1b()).Show();
+        }
+
+        private void Task1aClick(object sender, EventArgs e)
+        {
+            (new Task1a.Task1a()).Show();
+        }
+
+        private void Task2Click(object sender, EventArgs e)
+        {
+            (new Task2.Task2()).Show();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace cg_lab3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Loader());
         }
     }
 }
